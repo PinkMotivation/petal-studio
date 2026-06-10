17d829d001c2dd0b24a4fd5a3317c5232497288d
PinkMotivation <emorymayy@gmail.com>
2026-06-10 21:25:29 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-export const runtime = 'edge';
 'use client';
+export const runtime = 'edge';
 import React, { useState, useEffect } from 'react';
 import ShelfGrid from '../components/bookshelf/ShelfGrid';
 import JournalWorkspaceView from './journal/[id]/page';
