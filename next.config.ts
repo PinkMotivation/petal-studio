e09e1ea0f7a72e26aaab3ff7c487fdb3dcec08d7
PinkMotivation <emorymayy@gmail.com>
2026-06-10 20:56:53 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: { unoptimized: true },
   typescript: {
     // This stops strict type-checking errors from crashing your deployment
